aafe05cccb792ac2e574eaac7e3d4510e1bc1040
WilterD <wilterd18@gmail.com>
2022-12-24 12:39:46 -0400

    cambios

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import express from 'express';
-import ejs from 'ejs';
 import {dirname, join} from 'path'; 
 import {fileURLToPath} from 'url'; 
 
 import indexRoutes from './routes/index.js';
 
-const app = express();
+const app = express(); // referenciar a express
 
-const __dirname = dirname(fileURLToPath(import.meta.url)); 
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname,'views')); 
 app.set('view engine', 'ejs');
-
-app.use(indexRoutes)
-
-
-
-
-
+app.use(indexRoutes);
+app.use(express.static(join(__dirname,'public')));
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log('Server iniciado on port 3000');
 });
